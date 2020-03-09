761d49b1024d6dcc05c584b1f491d13bba7f0986
JeanMGirard <Jean.M.Girard@Outlook.com>
2020-03-09 09:58:24 -0400

    Changed directory structure

--- src/board/board.tsx
+++ src/board/board.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, { useContext } from "react";
 import {DragDropContext, Droppable, DropResult, DraggableLocation} from 'react-beautiful-dnd';
 
 import {KbColumn } from "../column/column";
